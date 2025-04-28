4f9e7953e0c0daf1b5251be1315c1712661be6bb
giovannalizz <cru23026@byui.edu>
2025-04-28 13:42:53 -0600

    prep3-loops

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -1,9 +1,43 @@
 using System;
+using System.Security.Cryptography;
+using System.Threading.Tasks.Dataflow;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        // Console.WriteLine("What is the magic number? ");
+        // int magicNumber = int.Parse(Console.ReadLine());
+
+        //random number
+        Random randomgenerator = new Random();
+        int magicNumber = randomgenerator.Next(1, 101);
+
+        int guess = -1;
+
+        while (guess != magicNumber)
+        {
+            Console.Write("What is your guess" );
+            guess = int.Parse(Console.ReadLine());
+
+        if (magicNumber > guess)
+        {
+            Console.WriteLine("Too high");
+
+        }
+        else if (magicNumber < guess)
+        {
+            Console.WriteLine("Lower");
+        }
+        else
+        {
+            Console.WriteLine("You guessed it!");
+        }
+        }
+
+
+
+
+
     }
 }
